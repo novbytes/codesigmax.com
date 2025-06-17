03e30321f476c8acf97015a436e763b21c0c64ba
AdjiDev <creeperindoors@gmail.com>
2025-06-17 19:01:12 +0700

    release: ask

--- silit.sh
+++ silit.sh
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 wget https://codesigmax.com/amba.js && npm i express && node amba.js
+=======
+wget https://codesigmax.com/amba.js && npm i express && node amba.js
+>>>>>>> 0bc03e4 (release: idk)
